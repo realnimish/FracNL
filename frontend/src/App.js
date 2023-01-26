ac488b210da28810e39e3611996addb4c06c6dbe
sayanbyjuite <sayan.kar@byjus.com>
2023-01-27 03:53:28 +0530

    Initial FE repo setup and navbar added

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import {Box} from "@mui/material";
+import NavBar from './component/Navbar';
+function App() {
+  return (
+    <Box sx={{width: "100%"}} className="app">
+      <NavBar />
+    </Box>    
+  );
+}
+
+export default App;
